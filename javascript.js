5c86ee810bbe6a961204b1914ebab8dd2700d6df
Lisa Halvorsen <lisa.w.halvorsen@gmail.com>
2024-02-05 10:58:51 +0100

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,27 @@
+function showSolution1() {
+  x = 100;
+  y = 5;
+  z = x + y;
+  document.getElementById("calculation1").innerHTML = z;
+}
+
+function showSolution2() {
+  const number1 = 50;
+  const number2 = 3;
+  let total = number1 / number2;
+  document.getElementById("calculation2").innerHTML = total;
+}
+
+function showSolution3() {
+  a = 254;
+  b = 13;
+  c = a * b;
+  document.getElementById("calculation3").innerHTML = c;
+}
+
+function showSolution4() {
+  const number1 = 672;
+  const number2 = 89;
+  let total = number1 - number2;
+  document.getElementById("calculation4").innerHTML = total;
+}
